82831189758cbf9ebe4336a465a704c870ed1751
emmanuel <kipronofb@gmail.com>
2020-07-07 04:49:43 +0300

    initial

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,102 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where,
+} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getModelSchemaRef,
+  patch,
+  put,
+  del,
+  requestBody,
+} from '@loopback/rest';
+import {User} from '../models';
+import {UserRepository} from '../repositories';
+
+export class UserController {
+  constructor(
+    @repository(UserRepository)
+    public userRepository : UserRepository,
+  ) {}
+
+  @post('/users/signup', {
+    responses: {
+      '200': {
+        description: 'User model instance',
+        content: {'application/json': {schema: getModelSchemaRef(User)}},
+      },
+    },
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(User, {
+            title: 'NewUser',
+            exclude: ['id'],
+          }),
+        },
+      },
+    })
+    user: Omit<User, 'id'>,
+  ): Promise<User> {
+    return this.userRepository.create(user);
+  }
+
+  @post('/users/login', {
+    responses: {
+      '200': {
+        description: 'User model instance',
+        content: {'application/json': {schema: getModelSchemaRef(User)}},
+      },
+    },
+  })
+  async login(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: {
+            title: 'Login',
+            type: 'object',
+            properties: {
+              email:{
+                type: "string"
+              },
+              password:{
+                type: "string"
+              }
+            }
+          },
+        },
+      },
+    })
+    credentials: Omit<User, 'id'>,
+  ): Promise<User> {
+    return this.userRepository.create(credentials);
+  }
+
+  @get('/users/{id}', {
+    responses: {
+      '200': {
+        description: 'User model instance',
+        content: {
+          'application/json': {
+            schema: getModelSchemaRef(User, {includeRelations: true}),
+          },
+        },
+      },
+    },
+  })
+  async findById(
+    @param.path.string('id') id: string,
+    @param.filter(User, {exclude: 'where'}) filter?: FilterExcludingWhere<User>
+  ): Promise<User> {
+    return this.userRepository.findById(id, filter);
+  }
+}
